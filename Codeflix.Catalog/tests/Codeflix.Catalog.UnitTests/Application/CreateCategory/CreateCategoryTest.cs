54f999ec49e47759e00f0f529dcbb22ed6031c6a
Rafael Silva e Santos <rafaelsesantos@gmail.com>
2023-01-09 11:43:01 -0300

    testes use case category

--- Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,32 +1,53 @@
-﻿using Codeflix.Catalog.Domain.Entity;
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Entity;
 using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Application.CreateCategory;
 using FluentAssertions;
 using Moq;
-using UseCases = Codeflix.Catalog.Application.UseCases.CreateCategory;
+using UseCases = Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
-namespace Codeflix.Catalog.UnitTests.Application.CreateCategory;
+namespace Codeflix.Catalog.UnitTests.Application;
+
+[Collection(nameof(CreateCategoryTestFixture))]
 public class CreateCategoryTest
 {
+
+    private readonly CreateCategoryTestFixture _fixture;
+
+    public CreateCategoryTest(CreateCategoryTestFixture fixture)
+    {
+        _fixture=fixture;
+    }
+
     [Fact(DisplayName = nameof(CreateCategory))]
     [Trait("Application", "CreateCategory Use Cases")]
     public async void CreateCategory()
     {
-        var repositoryMock = new Mock<ICategoryRepository>();
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
-        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
 
-        var input = new CreateCategoryInput("Name", "Description", true);
+        var repoMock = _fixture.GetRepositoryMock();
+        var uowMock  = _fixture.GetUnitOfWorkMock();
+
+        var useCase = new UseCases.CreateCategory(uowMock.Object, repoMock.Object);
+
+        var input = _fixture.GetInput();
 
         var output = await useCase.Handle(input, CancellationToken.None);
 
         output.Should().NotBeNull();
-        (output.Id != null && output.Id != default(Guid)).Should().BeTrue();
-        output.Name.Should().Be("Name");
-        output.Description.Should().Be("Description");
-        output.IsActive.Should().Be(true);
-        (output.CreatedAt != null && output.CreatedAt != default(DateTime)).Should.BeTrue();
-        repositoryMock.Verify(repo => repo.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
-        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+        output.Id.Should().NotBeEmpty();
+        output.Name.Should().Be(input.Name) ;
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        repoMock.Verify(repo => repo.Insert(
+            It.IsAny<Category>(), 
+            It.IsAny<CancellationToken>()), 
+            Times.Once);
+
+        uowMock.Verify(uow => uow.Commit(
+            It.IsAny<CancellationToken>()), 
+            Times.Once);
+
 
     }
 }
---
+++ Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using Moq;
+
+namespace Codeflix.Catalog.UnitTests.Application.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture> { }
+public class CreateCategoryTestFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var name = "";
+        while (name.Length < 3)
+            name = Faker.Commerce.Categories(1)[0];
+
+        if (name.Length > 255)
+            name = name[..255];
+
+        return name;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var description = Faker.Commerce.ProductDescription();
+
+        if (description.Length > 10000)
+            description = description[..10000];
+
+        return description;
+    }
+
+    public bool GetRandomIsActive() => (new Random().NextDouble() < 0.5);
+
+    public CreateCategoryInput GetInput() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomIsActive());
+
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();  
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+
+    
+}
