1d660fcb9e33b45cd694142d2651bfd918209748
Rafael Silva e Santos <rafaelsesantos@gmail.com>
2023-01-12 13:02:46 -0300

    Update CreateCategoryTest.cs

--- Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -1,7 +1,8 @@
-﻿using Codeflix.Catalog.Application.Interfaces;
+﻿using Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 using Codeflix.Catalog.Domain.Entity;
-using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.Domain.Exceptions;
 using Codeflix.Catalog.UnitTests.Application.CreateCategory;
+using Codeflix.Catalog.UnitTests.Domain.Entity.Category;
 using FluentAssertions;
 using Moq;
 using UseCases = Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
@@ -47,7 +48,54 @@ public class CreateCategoryTest
         uowMock.Verify(uow => uow.Commit(
             It.IsAny<CancellationToken>()), 
             Times.Once);
+    }
 
+    [Theory(DisplayName = nameof(ThrowWhenCantInstantiateAggregate))]
+    [Trait("Application", "Throw Error CreateCategory Use Cases")]
+    [MemberData(nameof(GetInvalidInputs))]
+    public async void ThrowWhenCantInstantiateAggregate(CreateCategoryInput input, string exceptionMessage)
+    {
 
+        var useCase = new UseCases.CreateCategory(
+            _fixture.GetUnitOfWorkMock().Object,
+            _fixture.GetRepositoryMock().Object);
+
+       Func<Task> task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should()
+            .ThrowAsync<EntityValidationException>()
+            .WithMessage(exceptionMessage);
+    }
+    
+    public static IEnumerable<object[]> GetInvalidInputs()
+    {
+        var fixture = new CreateCategoryTestFixture();
+        var invalidInputList = new List<object[]>();
+
+        //nome não pode ser null, não pode ser menor que 3 e maior que 255 caracteres
+        //descrição não pode ser null e tamanho máximo 10k
+
+        var invalidInputShortName = fixture.GetInput();
+        invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+
+        invalidInputList.Add(new object[] 
+        { 
+            invalidInputShortName,
+            "Name should have at least 3 characters"
+        });
+
+        var invalidInputLongName = fixture.GetInput();
+        invalidInputLongName.Name = "";
+
+        while (invalidInputLongName.Name.Length <= 250)
+            invalidInputLongName.Name = $"{invalidInputLongName.Name} {fixture.Faker.Commerce.ProductDescription()}";
+
+        invalidInputList.Add(new object[]
+        {
+            invalidInputLongName,
+            "Name should have no more than 250 characters"
+        });
+
+        return invalidInputList;
     }
 }
