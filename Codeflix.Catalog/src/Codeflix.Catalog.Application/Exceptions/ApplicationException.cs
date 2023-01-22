c8fda554bcf40f9b102e01fd57bc30f1424c3a78
Rafael Silva e Santos <rafaelsesantos@gmail.com>
2023-01-22 15:24:39 -0300

    not found exception

---
+++ Codeflix.Catalog/src/Codeflix.Catalog.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,7 @@
+﻿namespace Codeflix.Catalog.Application.Exceptions;
+public class ApplicationException : Exception
+{
+    public ApplicationException(string? message) : base(message)
+    {
+    }
+}
--- Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
+++ Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Moq;
 using UseCase = Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using Codeflix.Catalog.Application.Exceptions;
 
 namespace Codeflix.Catalog.UnitTests.Application.GetCategory;
 
@@ -40,5 +41,28 @@ public class GetCategoryTest
         output.IsActive.Should().Be(exampleCategory.IsActive);
         output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
 
+    }
+
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+    [Trait("Application", "Get Category - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesntExist()
+    {
+        var repo = _fixture.GetRepositoryMock();
+        var exampleGuid = Guid.NewGuid();
+
+        repo.Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new NotFoundException($"Category {exampleGuid} not found"));
+
+        var input = new UseCase.GetCategoryInput(exampleGuid);
+        var usecase = new UseCase.GetCategory(repo.Object);
+
+        var task = async () => await usecase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>();
+
+        repo.Verify(repo => repo.Get(
+          It.IsAny<Guid>(),
+          It.IsAny<CancellationToken>()),
+          Times.Once);
     }
 }
