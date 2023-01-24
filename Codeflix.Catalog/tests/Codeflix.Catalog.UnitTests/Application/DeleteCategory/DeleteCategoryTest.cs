7e059a9c46ed23d1c2a7d2985f2eae04a6e61322
Rafael Silva e Santos <rafaelsesantos@gmail.com>
2023-01-23 22:19:17 -0300

    inicio delete category

---
+++ Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Codeflix.Catalog.UnitTests.Application.DeleteCategory;
+
+using Codeflix.Catalog.Domain.Entity;
+using Moq;
+using UseCase = Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTest
+{
+    private readonly DeleteCategoryTestFixture _fixture;
+
+    public DeleteCategoryTest(DeleteCategoryTestFixture fixture)
+    {
+        _fixture=fixture;
+    }
+
+    [Fact(DisplayName = "")]
+    [Trait("Application", "Delete Category - Use Cases")]
+    public async void DeleteCategory()
+    {
+        var repo = _fixture.GetRepositoryMock();
+        var uow = _fixture.GetUnitOfWorkMock();
+        var validCategory = _fixture.GetValidCategory();
+        var input = new DeleteCategoryInput(validCategory.Id);
+        repo.Setup(x => x.Get(validCategory.Id, It.IsAny<CancellationToken>())).ReturnsAsync(validCategory);
+        var useCase = new DeleteCategory(repo, uow);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        repo.Verify(repo => repo.Get(
+          validCategory.Id,
+          It.IsAny<CancellationToken>()),
+          Times.Once);
+
+        repo.Verify(repo => repo.Delete(
+           validCategory,
+           It.IsAny<CancellationToken>()),
+           Times.Once);
+
+        uow.Verify(uow => uow.Commit(
+            It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
---
+++ Codeflix.Catalog/tests/Codeflix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using Moq;
+
+namespace Codeflix.Catalog.UnitTests.Application.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
+
+public class DeleteCategoryTestFixture : BaseFixture
+{
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+
+    public string GetValidCategoryName()
+    {
+        var name = "";
+        while (name.Length < 3)
+            name = Faker.Commerce.Categories(1)[0];
+
+        if (name.Length > 255)
+            name = name[..255];
+
+        return name;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var description = Faker.Commerce.ProductDescription();
+
+        if (description.Length > 10000)
+            description = description[..10000];
+
+        return description;
+    }
+
+    public Category GetValidCategory() => new(
+        GetValidCategoryName(),
+        GetValidCategoryDescription()
+        );
+}
