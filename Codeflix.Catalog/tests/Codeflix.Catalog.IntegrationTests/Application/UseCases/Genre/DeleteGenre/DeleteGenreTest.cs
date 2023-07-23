5dbc987c8b82bf533bb9a02e77bc57a44a30f563
Rafael Silva e Santos <rafaelsesantos@gmail.com>
2023-07-23 15:23:28 -0300

    finalização delete

--- Codeflix.Catalog/tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
+++ Codeflix.Catalog/tests/Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -1,7 +1,9 @@
 ﻿using Codeflix.Catalog.Application.Exceptions;
 using Codeflix.Catalog.Infra.Data.EF;
+using Codeflix.Catalog.Infra.Data.EF.Models;
 using Codeflix.Catalog.Infra.Data.EF.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using UseCase = Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
 
 namespace Codeflix.Catalog.IntegrationTests.Application.UseCases.Genre.DeleteGenre;
@@ -57,5 +59,41 @@ public class DeleteGenreTest
         await action.Should().ThrowAsync<NotFoundException>()
          .WithMessage($"Genre '{invalidId}' not found");
 
+    }
+
+    [Fact(DisplayName = nameof(DeleteWithRelations))]
+    [Trait("Integration/Application", "Delete Use Cases")]
+    public async Task DeleteWithRelations()
+    {
+        var genreExampleList = _fixture.GetExampleGenresList();
+        var targetGenre = genreExampleList[5];
+        var exampleCategories = _fixture.GetExampleCategoriesList();
+
+        var arrangeContext = _fixture.CreateDbContext();
+        await arrangeContext.AddRangeAsync(genreExampleList);
+        await arrangeContext.AddRangeAsync(exampleCategories);
+        
+        await arrangeContext.AddRangeAsync(
+            exampleCategories.Select(category => new GenresCategories(category.Id, targetGenre.Id)));
+
+        await arrangeContext.SaveChangesAsync();
+
+        var actContext = _fixture.CreateDbContext(true);
+        var useCase = new UseCase.DeleteGenre(new GenreRepository(actContext), new UnitOfWork(actContext));
+        var input = new UseCase.DeleteGenreInput(targetGenre.Id);
+        await useCase.Handle(input, CancellationToken.None);
+
+        var assertContext = _fixture.CreateDbContext(true);
+
+        var genreFromDb = await assertContext.Genres.FindAsync(targetGenre.Id);
+
+        var genresCategoriesFromDb = await assertContext
+            .GenresCategories
+            .AsNoTracking()
+            .Where(relation => relation.GenreId == targetGenre.Id)
+            .ToListAsync();
+
+        genreFromDb.Should().BeNull();
+        genresCategoriesFromDb.Should().HaveCount(0);
     }
 }
